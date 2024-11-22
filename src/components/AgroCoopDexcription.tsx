0a0ea3f3fd1190e603ae62ae63df7ef7ac36d175
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-22 16:54:37 -0300

    feat: add text section

--- src/components/AgroCoopDexcription.tsx
+++ src/components/AgroCoopDexcription.tsx
@@ -5,6 +5,12 @@ const AgroCoopDexcription = () => {
     <div className='w-screen flex items-center justify-center mt-12'>
     <div className='justify-start w-4/5'>
         <h1 className='text-green900 text-4xl text-left'>Quem somos?</h1>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Eveniet quis illo assumenda temporibus numquam, delectus ab nemo
+              neque! Animi molestiae maiores autem nemo, nihil reiciendis asperiores
+               dolor atque assumenda minima?</p>
+        </div>
     </div>
     </div>
     
