f8a0d60a157fd8c3fcbdfa497d87292fc056eb4c
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-25 14:03:24 -0300

    feat: description text component

--- src/components/AgroCoopDexcription.tsx
+++ src/components/AgroCoopDexcription.tsx
@@ -5,11 +5,23 @@ const AgroCoopDexcription = () => {
     <div className='w-screen flex items-center justify-center mt-12'>
     <div className='justify-start w-4/5'>
         <h1 className='text-green900 text-4xl text-left'>Quem somos?</h1>
-        <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Eveniet quis illo assumenda temporibus numquam, delectus ab nemo
-              neque! Animi molestiae maiores autem nemo, nihil reiciendis asperiores
-               dolor atque assumenda minima?</p>
+        <div className='text-black text-2xl'>
+          <p>A Agro Coop Solutions é uma startup criada por alunos de curssos de graduação da
+            Universidade Federal do Ceara, mas precisamente no campus de quixada, que tem como 
+            objetivo ajudar o desenvolvimento de cooperativas rurais atraves de soluções tecnologicas. 
+            A ideia surgiu no inicio de 2024 quando um grupo de alunos dos curssos de graduação 
+            viram uma grande dificuldade das cooperativas rurais da região em grenciamento de negocios,
+            visto que a maioria das cooperativas da região ainda fazem essa organização atraves de ferramentas
+            manuais como agendas e cadernos. Dessa forma percebemos que havia a oportunidade de criar
+            uma solução que teria inpacto positivo na comunidade e uma oportunidade de negocios também.
+            Assim surgiu a Agro Coop Solutions uma startup tecnologica com objetivo de ajudar as
+             cooperativas rurais e a produção agropecuaria brasileira.<br />
+             Atualmente nosso equipe é formada por cinco integrantes, todos alunos da Universidade 
+             Federal do Ceara, O nosso CEO Gabriel Braga aluno do cursso de engenharia da computação, 
+             o Tech Lead Victor Gomes da Costa aluno de engenharia de computação, o nosso QA Matheus Moreira 
+             também aluno de engenharia da computação, e os desenvolvedores Pedro Braga aluno do cursso de engenharia da computação
+             e Andre Torquato aluno de sistemas de informção.
+          </p>
         </div>
     </div>
     </div>
