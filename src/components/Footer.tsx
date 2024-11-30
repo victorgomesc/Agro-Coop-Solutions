029259f118d440da303e18602f50b71dd3adaae1
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-11-29 22:38:57 -0300

    feat: implement footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,10 @@ const Footer = () => {
   return (
     <div className='footer'>
       <h1>footer</h1>
+      <h1>Github</h1>
+      <h1>Linkedln</h1>
+      <h1>Instagram</h1>
+      <h1>x</h1>
     </div>
   )
 }
